6481b37dad3dac27f5535c50056265d27d0a80e7
BondoTsaguria <bondotsaguria@gmail.com>
2023-12-13 19:50:46 +0400

    add static design for navbar

--- src/app/core/modules/app.module.ts
+++ src/app/core/modules/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from '../App/app.component';
+import { NavbarComponent } from 'src/app/features/navbar/navbar.component';
+import { GeneratorComponent } from 'src/app/features/generator/generator.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, GeneratorComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
