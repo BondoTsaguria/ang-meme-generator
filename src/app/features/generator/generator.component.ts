1446418eac071b9d8802bf8b8f5f98405c0b48c6
BondoTsaguria <bondotsaguria@gmail.com>
2023-12-18 18:44:38 +0400

    add logic to insert chosen img inside canvas

--- src/app/features/generator/generator.component.ts
+++ src/app/features/generator/generator.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-generator',
   templateUrl: './generator.component.html',
-  styleUrls: ['./generator.component.css']
+  styleUrls: ['./generator.component.css'],
 })
-export class GeneratorComponent {
+export class GeneratorComponent implements OnInit {
+  @ViewChild('memeCanvas', { static: false }) myCanvas: any;
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  preview(e: any) {
+    let canvas = this.myCanvas.nativeElement;
+    let ctx = canvas.getContext('2d');
+
+    let render = new FileReader();
+    render.readAsDataURL(e.target.files[0]);
+    render.onload = function (event) {
+      const img = new Image();
+      img.src = event.target?.result as string;
+
+      img.onload = function () {
+        ctx.drawImage(img, 25, 150, 500, 500);
+      };
+    };
+  }
 }
